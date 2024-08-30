e18b36ab4a35faa9141cf6196e5a71c7bd1bea48
baba240491 <112175704+baba240491@users.noreply.github.com>
2024-08-30 10:32:45 +0200

    le code marche car le probleme est due au repliqua

--- posts/index.js
+++ posts/index.js
@@ -8,17 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.use(cors(
-
-  {
-    origin: 'http://localhost:3000',  // Frontend URL
-    credentials: true,
-    methods: ['GET', 'POST', 'OPTIONS'],  // Ensure OPTIONS is allowed for preflight requests
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Include any headers that your frontend is sending
-    optionsSuccessStatus: 200,
-  }
-
-));
+app.use(cors());
 
 
 const posts = {};
